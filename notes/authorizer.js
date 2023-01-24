17a85435a4b599f970a443de189ec5fa1868f501
AdamAtamnia <adam18102002.aa@gmail.com>
2023-01-24 08:04:55 -0500

    add access control with cognito user pool

--- notes/authorizer.js
+++ notes/authorizer.js
@@ -1,3 +1,14 @@
+const { CognitoJwtVerifier } = require("aws-jwt-verify")
+const COGNITO_USERPOOL_ID = process.env.COGNITO_USERPOOL_ID
+const COGNITO_WEB_CLIENT_ID = process.env.COGNITO_WEB_CLIENT_ID
+
+
+const jwtVerifier = CognitoJwtVerifier.create({
+  userPoolId: COGNITO_USERPOOL_ID,
+  tokenUse: "id", // type of token (for us is id token) id or access
+  clientId: COGNITO_WEB_CLIENT_ID, // client id of the userpool client: used to interact with the user pool and perform actions such as user authentication and authorization.
+})
+
 const generatePolicy = (principalId, effect, resource) => {
   const authResponse = {}
 
@@ -25,18 +36,35 @@ const generatePolicy = (principalId, effect, resource) => {
   return authResponse
 }
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
   // by default the token will be taken from authorization in the header
-  const token = event.authorizationToken // "allow" or "deny"
-  switch (token) {
-    case "allow":
-      callback(null, generatePolicy("user", "Allow", event.methodArn)) // api gateway gives this as arn of the incomming method request
-      break;
-    case "deny":
-      callback(null, generatePolicy("user", "Deny", event.methodArn)) 
-      break;
-    default:
-      callback("Error: Invalid token") 
-      break;
+  const token = event.authorizationToken 
+  console.log(token)
+  try {
+    const payload = await jwtVerifier.verify(token)
+    console.log(JSON.stringify(payload))
+
+    callback(null, generatePolicy("user", "Allow", event.methodArn))
+  } catch (error) {
+    callback("Error: Invalid token")
   }
+  
 }
+
+
+// OLD EXERCISE
+// exports.handler = (event, context, callback) => {
+//   // by default the token will be taken from authorization in the header
+//   const token = event.authorizationToken // "allow" or "deny", normal this is a jwt token
+//   switch (token) {
+//     case "allow":
+//       callback(null, generatePolicy("user", "Allow", event.methodArn)) // api gateway gives this as arn of the incomming method request
+//       break;
+//     case "deny":
+//       callback(null, generatePolicy("user", "Deny", event.methodArn)) 
+//       break;
+//     default:
+//       callback("Error: Invalid token") 
+//       break;
+//   }
+// }
